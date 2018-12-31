9cadb3180d1ec33b7b51417ea6f0f5d8970c89e2
B͈̤̖̪̪̱ͅl̯̯̮̼͎̬͚̳̩̖̲u̜̼͉͈̠b͙̬̘̙̱̗̲͙b͍̞̬̬͓̼l̰̪͖̯̼̟̟͈̖͕̜̱̜ͅl̻̗͔̝̭̰͚͇̯̥ <34196146+blubbll@users.noreply.github.com>
2018-12-31 06:25:34 +0100

    added callback, fixed directories

--- ftp.js
+++ ftp.js
@@ -1,29 +1,29 @@
-function upload(credentials,pathToRemoteFile,pathToLocalFile, callback){
-  var Client = require('ftp');
-  var options = {
-      host: credentials.host,
-      port: credentials.port,
-      username: credentials.username,
-      password: credentials.password
+function upload(credentials, pathToLocalFile, pathToRemoteFile, callback) {
+    var Client = require('ftp');
+    var options = {
+        host: credentials.host,
+        port: credentials.port,
+        username: credentials.username,
+        password: credentials.password
     };
-  var c = new Client();
+    var c = new Client();
 
-  c.on('ready', function() {
-    c.mkdir(remoteDirectory, true, function(err) {
-    if (err) throw err;
-    else{
-      c.put(localDirectory+localFile,remoteDirectory+remoteFile, function(err) {
-        if (err){throw err;}
-         if(callback!==void 0) callback();
-        c.end();
-      });
-    }
-  });
-});
-c.on('error',function(err){
-  console.log(err);
-});
-c.connect(options);
-
+    c.on('ready', function() {
+        c.mkdir(pathToRemoteFile, true, function(err) {
+            if (err) throw err;
+            else {
+                c.put(pathToLocalFile, pathToRemoteFile, function(err) {
+                    if (err) {
+                        throw err;
+                    }
+                    if (callback !== void 0) callback();
+                    c.end();
+                });
+            }
+        });
+    });
+    c.on('error', function(err) {
+        console.log(err);
+    });
+    c.connect(options);
 }
-module.exports.upload = upload;
